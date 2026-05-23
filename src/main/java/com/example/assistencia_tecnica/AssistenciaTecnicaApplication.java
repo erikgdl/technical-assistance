81f42101a325b9419af75de437f09b6e16abe242
Erik Gabriel <erikgab10@gmail.com>
2026-05-23 17:36:38 -0400

    Initial Commit

---
+++ src/main/java/com/example/assistencia_tecnica/AssistenciaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.assistencia_tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistenciaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistenciaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/assistencia_tecnica/AssistenciaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assistencia_tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistenciaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
