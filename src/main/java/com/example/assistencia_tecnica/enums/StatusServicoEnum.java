e45712f0489f6343e3312a05edc8623b4d59a499
Erik Gabriel <erikgab10@gmail.com>
2026-05-23 17:57:25 -0400

    feat: adiciona pacote controller com os status dos serviços

---
+++ src/main/java/com/example/assistencia_tecnica/enums/StatusServicoEnum.java
@@ -0,0 +1,12 @@
+package com.example.assistencia_tecnica.enums;
+
+public enum StatusServicoEnum {
+    ABERTA,
+    EM_ANALISE,
+    AGUARDANDO_APROVACAO_CLIENTE,
+    APROVADA,
+    REJEITADA,
+    EM_MANUTENCAO,
+    CONCLUIDA,
+    ENTREGUE
+}
