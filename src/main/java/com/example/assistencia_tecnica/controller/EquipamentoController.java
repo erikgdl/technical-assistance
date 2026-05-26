1a512c7c96bb6097cb855f4a00857ee9025a7cb0
Erik Gabriel <erikgab10@gmail.com>
2026-05-25 23:28:10 -0400

    chore: atualiza Endpoint Post de criar equipamento

--- src/main/java/com/example/assistencia_tecnica/controller/EquipamentoController.java
+++ src/main/java/com/example/assistencia_tecnica/controller/EquipamentoController.java
@@ -1,9 +1,11 @@
 package com.example.assistencia_tecnica.controller;
 
+import com.example.assistencia_tecnica.database.model.EquipamentoEntity;
 import com.example.assistencia_tecnica.dto.EquipamentoDto;
 import com.example.assistencia_tecnica.service.EquipamentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +18,8 @@ public class EquipamentoController {
     private final EquipamentoService equipamentoService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void criarEquipamento(@RequestBody @Validated EquipamentoDto equipamentoDto) {
-        equipamentoService.criarEquipamento(equipamentoDto);
+    public ResponseEntity<EquipamentoEntity> criarEquipamento(@RequestBody @Validated EquipamentoDto dto) {
+        EquipamentoEntity novoEquipamento = equipamentoService.criarEquipamento(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoEquipamento);
     }
 }
