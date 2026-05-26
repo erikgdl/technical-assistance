dacdf951a56ac2f7c1f1f7120901e0384b2b4717
Erik Gabriel <erikgab10@gmail.com>
2026-05-25 23:18:35 -0400

    chore: excluir classes ServicoRealizadoRepository

--- src/main/java/com/example/assistencia_tecnica/database/repository/ServicoRealizadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.assistencia_tecnica.database.repository;
-
-import com.example.assistencia_tecnica.database.model.ServicoRealizadoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ServicoRealizadoRepository extends JpaRepository<ServicoRealizadoEntity, Long> {
-}
