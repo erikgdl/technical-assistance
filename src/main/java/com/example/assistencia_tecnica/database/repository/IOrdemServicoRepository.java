1f3aae2d359f0eb22b00ce305875a2febb0e43d0
Erik Gabriel <erikgab10@gmail.com>
2026-05-25 23:16:39 -0400

    chore: atualiza parâmetro de Long para UUID

--- src/main/java/com/example/assistencia_tecnica/database/repository/IOrdemServicoRepository.java
+++ src/main/java/com/example/assistencia_tecnica/database/repository/IOrdemServicoRepository.java
@@ -1,10 +1,9 @@
 package com.example.assistencia_tecnica.database.repository;
 
-import com.example.assistencia_tecnica.database.model.ClienteEntity;
 import com.example.assistencia_tecnica.database.model.OrdemServicoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface IOrdemServicoRepository extends JpaRepository<OrdemServicoEntity, Long> {
+public interface IOrdemServicoRepository extends JpaRepository<OrdemServicoEntity, UUID> {
 }
