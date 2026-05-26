6d270101da59c300823271d50fd843ee6c749cbc
Erik Gabriel <erikgab10@gmail.com>
2026-05-25 23:14:39 -0400

    chore: adiciona novo metodo findById no IEquipamentoRepository

--- src/main/java/com/example/assistencia_tecnica/database/repository/IEquipamentoRepository.java
+++ src/main/java/com/example/assistencia_tecnica/database/repository/IEquipamentoRepository.java
@@ -1,10 +1,12 @@
 package com.example.assistencia_tecnica.database.repository;
 
-import com.example.assistencia_tecnica.database.model.ClienteEntity;
 import com.example.assistencia_tecnica.database.model.EquipamentoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IEquipamentoRepository extends JpaRepository<EquipamentoEntity, UUID> {
+
+    Optional<EquipamentoEntity> findById(Long id);
+
 }
