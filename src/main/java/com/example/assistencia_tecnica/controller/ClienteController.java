87570a2ee337a7ca58c5d93466f7fd1dd02d36b9
Erik Gabriel <erikgab10@gmail.com>
2026-05-25 23:27:49 -0400

    chore: atualiza Endpoint Post de cadastrar cliente

--- src/main/java/com/example/assistencia_tecnica/controller/ClienteController.java
+++ src/main/java/com/example/assistencia_tecnica/controller/ClienteController.java
@@ -1,11 +1,13 @@
 package com.example.assistencia_tecnica.controller;
 
+import com.example.assistencia_tecnica.database.model.ClienteEntity;
 import com.example.assistencia_tecnica.dto.ClienteDto;
 import com.example.assistencia_tecnica.exception.BadRequestException;
 import com.example.assistencia_tecnica.service.ClienteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +20,21 @@ public class ClienteController {
     private final ClienteService clienteService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void criarCliente(@Valid @RequestBody ClienteDto clienteDto) throws BadRequestException {
-        clienteService.criarCliente(clienteDto);
+    public ResponseEntity<ClienteEntity> cadastrarCliente(@RequestBody @Valid ClienteDto dto) throws BadRequestException {
+        ClienteEntity novoCliente = clienteService.criarCliente(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoCliente);
     }
+
+    /*
+    @GetMapping
+    public ResponseEntity<List<ClienteEntity>> listarTodos() {
+        return ResponseEntity.ok(clienteService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteEntity> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+    */
+
 }
