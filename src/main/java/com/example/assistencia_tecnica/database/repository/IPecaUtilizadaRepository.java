07fb9a098147e36db52e9eb6c42fb3134ae3e5d7
Erik Gabriel <erikgab10@gmail.com>
2026-05-25 23:00:00 -0400

    feat: adiciona classe de Peça Utilizada no pacote repository com todos os repositórios impl JPA

---
+++ src/main/java/com/example/assistencia_tecnica/database/repository/IPecaUtilizadaRepository.java
@@ -0,0 +1,15 @@
+package com.example.assistencia_tecnica.database.repository;
+
+import com.example.assistencia_tecnica.database.model.PecaUtilizadaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IPecaUtilizadaRepository extends JpaRepository<PecaUtilizadaEntity, Long>{
+
+    @Query("SELECT p FROM PecaUtilizadaEntity p WHERE p.ordemServicoId.id = :osId")
+    List<PecaUtilizadaEntity> findByOrdemServicoId(@Param("osId") UUID ordemServicoId);
+}
