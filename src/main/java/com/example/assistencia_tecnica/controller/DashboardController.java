aaa0eb4b86c7c28bf83c6ce28dce75d116e7138a
Erik Gabriel <erikgab10@gmail.com>
2026-05-27 16:01:29 -0400

    chore: exclui imports não utilizada no projeto

--- src/main/java/com/example/assistencia_tecnica/controller/DashboardController.java
+++ src/main/java/com/example/assistencia_tecnica/controller/DashboardController.java
@@ -1,20 +1,11 @@
 package com.example.assistencia_tecnica.controller;
 
-import com.example.assistencia_tecnica.database.model.PecaEntity;
 import com.example.assistencia_tecnica.dto.DashboardResponseDto;
-import com.example.assistencia_tecnica.dto.PecaDto;
-import com.example.assistencia_tecnica.exception.NotFoundException;
 import com.example.assistencia_tecnica.service.DashboardService;
-import com.example.assistencia_tecnica.service.PecaService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("v1/dashboard")
