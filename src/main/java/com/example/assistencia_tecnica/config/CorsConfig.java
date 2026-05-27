eecf78c21cd6ac662c346fd8c91c883940aa69a9
Erik Gabriel <erikgab10@gmail.com>
2026-05-27 16:19:09 -0400

    feat: adiciona notação configuration

--- src/main/java/com/example/assistencia_tecnica/config/CorsConfig.java
+++ src/main/java/com/example/assistencia_tecnica/config/CorsConfig.java
@@ -1,8 +1,10 @@
 package com.example.assistencia_tecnica.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
