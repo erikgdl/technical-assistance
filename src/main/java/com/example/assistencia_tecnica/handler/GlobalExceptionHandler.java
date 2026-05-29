f478c7ad860462ed59bb366365322b67e923f4b7
Erik Gabriel <erikgab10@gmail.com>
2026-05-28 20:26:52 -0400

    feat: adiciona métodos no GlobalExceptionHandler

--- src/main/java/com/example/assistencia_tecnica/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/assistencia_tecnica/handler/GlobalExceptionHandler.java
@@ -5,8 +5,13 @@ import com.example.assistencia_tecnica.exception.ErrorResponse;
 import com.example.assistencia_tecnica.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,6 +20,21 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex){
         ErrorResponse response = ErrorResponse.builder()
                 .message(ex.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        String detalhesErro = ex.getBindingResult().getFieldErrors().stream()
+                .map(erro -> erro.getField() + ": " + erro.getDefaultMessage())
+                .collect(Collectors.joining(" | "));
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message("Dados inválidos: " + detalhesErro)
                 .status(HttpStatus.BAD_REQUEST.value())
                 .build();
 
@@ -42,4 +62,24 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> tratarRotaNaoEncontrada(NoResourceFoundException ex) {
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message("A rota que você tentou acessar não existe no servidor. Verifique a URL.")
+                .status(HttpStatus.NOT_FOUND.value()) // Retorna 404
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> tratarErroDeTipoNaUrl(MethodArgumentTypeMismatchException ex) {
+        ErrorResponse response = ErrorResponse.builder()
+                .message("O valor informado na URL tem um formato inválido. Verifique se é um ID ou UUID correto.")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 }
